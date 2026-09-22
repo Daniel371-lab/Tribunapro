a090bf0fe9bc6607067c8494eb3d808bbe7e47d9
daniel371-lab <jplabscreator@gmail.com>
2026-09-22 01:42:10 +0000

    Ver muestra grande

--- lib/core/services/firestore_service.dart
+++ lib/core/services/firestore_service.dart
@@ -43,7 +43,7 @@ class FirestoreService {
             .toList());
   }
 
-  Stream<List<Partido>> historial({int limite = 36}) {
+  Stream<List<Partido>> historial({int limite = 100}) {
     return _partidos
         .where('publicado', isEqualTo: true)
         .where('finalizado', isEqualTo: true)
